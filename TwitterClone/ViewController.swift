5b582c0f145a2471e8cc9c32d3d2c5687e3c6b51
nikolas <nikoagge@gmail.com>
2019-06-27 16:25:52 +0300

    Initial Commit

---
+++ TwitterClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TwitterClone
+//
+//  Created by Nikolas on 27/06/2019.
+//  Copyright © 2019 Nikolas Aggelidis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
